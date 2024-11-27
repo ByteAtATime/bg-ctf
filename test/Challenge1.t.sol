7cef53f624226945073dcc94c8bff1aa9fc9e3aa
ByteAtATime <byteatatime@proton.me>
2024-11-26 20:24:04 -0800

    first challenge

---
+++ test/Challenge1.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Challenge1} from "../src/Challenge1.sol";
+import {NFTFlags} from "../src/NFTFlags.sol";
+
+contract Challenge1Test is Test {
+    Challenge1 challenge1;
+    NFTFlags nftFlags;
+
+    function setUp() public {
+        nftFlags = new NFTFlags(address(this));
+        nftFlags.enable();
+        challenge1 = new Challenge1(address(nftFlags));
+        nftFlags.addAllowedMinter(address(challenge1));
+    }
+
+    function test_challenge1() public {
+        challenge1.registerTeam("Team Name", 2);
+        
+        assertTrue(nftFlags.hasMinted(address(this), 1));
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external pure returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+}
